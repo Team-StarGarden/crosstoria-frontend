43c5a49e079caeaeee8a77ef07e4f523de4c3111
Alex4386 <psh010209@gmail.com>
2019-12-26 19:10:52 +0900

    add: several stuff
    Co-authored-by: Doro-nyong <doronyong94@gmail.com>
    Co-authored-by: Basix <basix@basix.tech>
    Co-authored-by: PieSignal <insiro@naver.com>
    Co-authored-by: Joosung Park <admin@slowmotion.dev>
    Ate-popcorn-by: RanolP <public.ranolp@gmail.com>

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Login: React.FC = () => {
+  return (
+    <>
+      AyeDee : 
+      <input type="text"/>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/utils/User.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export type NoUser = {}
+
+export interface LocalUser {
+  id: number;
+  displayName: string;
+  age: number;
+  gender: string;
+}
+
+export type ContextualUser = LocalUser | NoUser
+
+export function isLocalUser(user: ContextualUser): user is LocalUser {
+  return "name" in user
+}
+
+export const defaultState: ContextualUser = {}
+
+export const UserContext = React.createContext<ContextualUser>(defaultState);
