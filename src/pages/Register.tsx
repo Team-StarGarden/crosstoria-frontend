f5a298742f37b1129780c348198c7704e2a27fb4
Basix <basix@basix.tech>
2019-12-30 00:30:02 +0900

    rename: BtnWrapper

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { jsx, css } from '@emotion/core';
 import { Button } from '../components/Button';
-import { BtnWrapper } from '../components/Button/BtnWrapper';
+import { ButtonWrapper } from '../components/Button/ButtonWrapper';
 import { TextInput } from '../components/Input/TextInput';
 import { mapBackground, centerContent, withGlobalOptions } from '../components/GlobalStyles';
 
@@ -13,7 +13,7 @@ import * as styles from '../constants';
 export const Register: React.FC = () => {
   return (
     <React.Fragment>
-      <BtnWrapper position="top-left" padding="x-small"><Button size="large"><icons.ArrowLeft /></Button></BtnWrapper>
+      <ButtonWrapper position="top-left" padding="x-small"><Button size="large"><icons.ArrowLeft /></Button></ButtonWrapper>
       <form css={css`width: calc(100% - 2 * ${styles.SPACE_XXL}); max-width: 272px;`}>
         <h1 css={css`${styles.TEXT_XL}; text-align: center;`}>가입 신청</h1>
         <h2 css={css`${styles.TEXT_L}; text-align: center;`}>연합원 기본 정보 입력</h2>
