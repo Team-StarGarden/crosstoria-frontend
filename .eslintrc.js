ff4a36bf647215b8940946d45d020459fc12f5ee
Basix <basix@basix.tech>
2019-12-30 01:16:57 +0900

    chore: improve linting config
    - add husky for commit-time check
    - add eslint prettier config and plugin to ensure conventions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,25 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-        'prettier/react'
-    ],
-    env: {
-        browser: true
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {
+    'react/prop-types': ['off'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    rules: {
-        indent: ['error', 2],
-        // 'linebreak-style': ['error', 'unix'],
-        quotes: ['error', 'single'],
-        semi: ['error', 'always'],
-        'react/prop-types': ['off']
-    },
-    settings: {
-        react: {
-            version: 'detect'
-        }
-    }
+  },
 };
