e0cd19b118baa206aff3af5b1dcd2e515e2b92fc
PieSignal <insiro@naver.com>
2019-12-30 03:02:47 +0900

    Separate sidebar components
    Co-authored-by: Basix <basix@basix.tech>

---
+++ src/components/SideBar/HashTagList.tsx
@@ -0,0 +1,17 @@
+/** @jsx jsx */
+
+import React from 'react';
+import { jsx } from '@emotion/core';
+
+export const HashTagList: React.FC = () => {
+  return (
+    <React.Fragment>
+      #마타리_결혼식
+      <br />
+      #스탤은_자유로운_몸이에요
+      <br />
+      #이벤트_참여
+    </React.Fragment>
+  );
+};
+export default HashTagList;
