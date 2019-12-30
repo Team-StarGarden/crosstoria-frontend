40fabb109e6123e802e29ac3913bce7296e8aaf4
Doro-nyong / 도로뇽 <doronyong94@gmail.com>
2019-12-31 01:41:05 +0900

    add: onClick props

--- src/components/CharacterProfile/CharacterProfileImage.tsx
+++ src/components/CharacterProfile/CharacterProfileImage.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, css } from '@emotion/core';
+import { jsx, css, SerializedStyles } from '@emotion/core';
 import styled from '@emotion/styled';
 import React from 'react';
 import * as styles from '../../constants';
@@ -11,14 +11,18 @@ type CharacterProfileImageProps = {
   src: string;
   /* 프로필의 크기를 설정합니다. */
   size: 'x-large' | 'large' | 'regular' | 'small' | 'x-small';
-}
+  /* 추가하고 싶은 CSS가 있다면, 해당 props를 사용합니다. */
+  additionalCSS?: SerializedStyles;
+  /* 클릭해서 발생하는 이벤트가 있다면, 해당 props를 사용합니다. */
+  onClick?: () => void;
+};
 
 const CharacterProfileImageStyle = css`
   ${styles.TRANSITION_SLOW}
   transition-property: border, filter;
 
   z-index: -1;
-  
+
   border-radius: 100%;
 
   &.x-large {
@@ -46,8 +50,9 @@ const CharacterProfileImageStyle = css`
     height: 32px;
   }
 
-  a:focus > &, a:hover > & {
-    filter: brightness(.5);
+  a:focus > &,
+  a:hover > & {
+    filter: brightness(0.5);
 
     &.x-large {
       filter: none;
@@ -55,12 +60,18 @@ const CharacterProfileImageStyle = css`
   }
 `;
 
-export const CharacterProfileImage: React.FC<CharacterProfileImageProps> = ({src, size}: CharacterProfileImageProps) => {
+export const CharacterProfileImage: React.FC<CharacterProfileImageProps &
+  React.DetailedHTMLProps<
+    React.ImgHTMLAttributes<HTMLImageElement>,
+    HTMLImageElement
+  >> = ({ src, size, additionalCSS, onClick, ...props }) => {
   return (
-    <img 
+    <img
       src={src}
-      css={CharacterProfileImageStyle}
+      css={css`${CharacterProfileImageStyle} ${additionalCSS}`}
       className={size}
+      onClick={onClick}
+      {...props}
     />
   );
 };
