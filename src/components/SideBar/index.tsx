0d74e45b958c4425eb6141e7a89cad3b001d4f47
Doro-nyong / 도로뇽 <doronyong94@gmail.com>
2019-12-31 01:42:29 +0900

    add: SideBar toggle button in mobile

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -4,25 +4,38 @@ import React from 'react';
 import { jsx, css } from '@emotion/core';
 
 import * as styles from '../../constants';
-import UserSummary from './CurrentChar';
+import CurrentChar from './CurrentChar';
 import HashTagList from './HashTagList';
 import PlaceNav from './PlaceNav';
 import BottomSetting from './BottomSetting';
 
+const SideBarStyle = css`
+  z-index: 20;
+
+  ${styles.TRANSITION_SLOW}
+  transition-property: left;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 256px;
+
+  background: ${styles.BROWN_300};
+
+  ${styles.MEDIAQUERY_UNDER_R} {
+    left: -256px;
+
+    body.sidebar-opened & {
+      left: 0;
+    }
+  }
+`;
+
 export const SideBar: React.FC = () => {
   return (
-    <div
-      css={css`
-        position: fixed;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        width: 256px;
-
-        background: ${styles.BROWN_300};
-      `}
-    >
-      <UserSummary characterName="캐릭터의 이름" characterJob="캐릭터의 직업" />
+    <div css={SideBarStyle}>
+      <CurrentChar characterName="캐릭터의 이름" characterJob="캐릭터의 직업" />
       <HashTagList />
       <PlaceNav />
       <BottomSetting />
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,14 +2,53 @@
 
 import React from 'react';
 import { jsx, css } from '@emotion/core';
+
+import * as styles from '../constants';
 import { SideBar } from '../components/SideBar';
+import { SideBarOnButton } from '../components/SideBar/SideBarOnButton';
+
+import ResponsiveLogo from '../components/ResponsiveLogo';
 
 export const Banner: React.FC = () => {
-  return <React.Fragment>banner</React.Fragment>;
+  return (
+    <div
+      css={css`
+        height: 250px;
+        background-image: ${styles.IMG_BG_WORLDMAP};
+        background-size: cover;
+        background-position: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      `}
+    >
+      <ResponsiveLogo width="156px" />
+    </div>
+  );
 };
 
 export const Main: React.FC = () => {
-  return <SideBar />;
+  return (
+    <React.Fragment>
+      <SideBar />
+      <div
+        css={css`
+          position: relative;
+          ${styles.TRANSITION_SLOW}
+          transition-property: margin-left;
+
+          margin-left: 256px;
+
+          ${styles.MEDIAQUERY_UNDER_R} {
+            margin-left: 0;
+          }
+        `}
+      >
+        <SideBarOnButton />
+        <Banner />
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Main;
