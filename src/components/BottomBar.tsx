c2d23ceba58d3cd43a40f4c71f6bf016facb8e1c
Doro-nyong / 도로뇽 <doronyong94@gmail.com>
2020-01-03 19:25:28 +0900

    add: media query at BottomBar

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -12,7 +12,17 @@ export const BottomButton: React.FC = ({ children }) => {
     <Button
       size="regular"
       css={css`
-        margin-right: ${styles.SPACE_R};
+        margin-right: ${styles.SPACE_XS};
+
+        ${styles.MEDIAQUERY_OVER_R} {
+          &:last-of-type {
+            margin-right: ${styles.SPACE_S};
+          }
+        }
+
+        ${styles.MEDIAQUERY_UNDER_R} {
+          margin: 0 calc(10vw - 24px);
+        }
       `}
     >
       {children}
@@ -24,19 +34,32 @@ export const BottomBar: React.FC = () => {
   return (
     <div
       css={css`
+        ${styles.TRANSITION_SLOW};
+        transition-property: left, padding-left;
+
         position: fixed;
         left: 256px;
         right: 0px;
         bottom: 0px;
+
+        padding: ${styles.SPACE_S} 0;
+
         height: 80px;
+        
         background: ${styles.BROWN_100};
+
+        ${styles.MEDIAQUERY_OVER_R} {
+          padding-left: ${styles.SPACE_S};
+        }
+
+        ${styles.MEDIAQUERY_UNDER_R} {
+          left: 0;
+        }
       `}
     >
       <div
         css={css`
-          padding-left: ${styles.SPACE_R};
           position: absolute;
-          left: 0;
         `}
       >
         <BottomButton>
