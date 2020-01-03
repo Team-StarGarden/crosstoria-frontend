c2ef008cbfc0d3df5c7dd15e4530e0eb5db5936e
Doro-nyong / 도로뇽 <doronyong94@gmail.com>
2020-01-04 00:55:52 +0900

    fix: add additionalCSS props to ResponsiveLogo

--- src/components/ResponsiveLogo.tsx
+++ src/components/ResponsiveLogo.tsx
@@ -1,22 +1,33 @@
 /** @jsx jsx */
 
-import { jsx, css } from '@emotion/core';
+import { jsx, css, SerializedStyles } from '@emotion/core';
 
 import logo from '../assets/logo.png';
 import logoWithTypo from '../assets/logo_withtypo.png';
 
 interface ResponsiveLogoProps {
   width?: string | number;
+  additionalCSS?: SerializedStyles;
 }
 
-const ResponsiveLogo: React.FC<ResponsiveLogoProps> = ({ width }) => (
-  <img
-    src={logoWithTypo}
-    css={css({ width: width || '100%' })}
-    alt="Logo"
-    srcSet={`${logoWithTypo} 617w, ${logo} 617w`}
-    sizes="(min-width: 480px) 617px, (max-width: 480px) 617px"
-  />
-);
+const ResponsiveLogo: React.FC<ResponsiveLogoProps> = ({
+  width,
+  additionalCSS,
+  ...props
+}) => {
+  return (
+    <img
+      src={logoWithTypo}
+      css={css`
+        width: ${width || '100%'};
+        ${additionalCSS}
+      `}
+      alt="Logo"
+      srcSet={`${logoWithTypo} 617w, ${logo} 617w`}
+      sizes="(min-width: 480px) 617px, (max-width: 480px) 617px"
+      {...props}
+    />
+  );
+};
 
 export default ResponsiveLogo;
