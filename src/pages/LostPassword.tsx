77a94b6da6ad74884a76d46cd672cd3b17580174
Doro-nyong / 도로뇽 <doronyong94@gmail.com>
2020-01-04 00:56:30 +0900

    fix: style of Login and LostPassword

--- src/pages/LostPassword.tsx
+++ src/pages/LostPassword.tsx
@@ -2,17 +2,21 @@
 
 import React from 'react';
 import { TextInput } from '../components/Input/TextInput';
-import * as styles from '../constants';
+
 import { jsx, css } from '@emotion/core';
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 import { Button } from '../components/Button';
+import { ButtonWrapper } from '../components/Button/ButtonWrapper';
 
 import {
   mapBackground,
   centerContent,
   withGlobalOptions,
 } from '../components/GlobalStyles';
+
+import * as icons from '../assets/Icons';
+import * as styles from '../constants';
 
 export const SigninButton = styled(Link)`
   ${styles.TEXT_S};
@@ -30,8 +34,9 @@ export const PageHint: React.FC = () => {
   return (
     <div
       css={css`
-        text-align: center;
-      `}
+      margin: 0 auto;
+      text-align: center;
+    `}
     >
       <h1
         css={css`
@@ -41,41 +46,50 @@ export const PageHint: React.FC = () => {
         비밀번호 재설정
       </h1>
       {/*TODO: need to prevent breakRow */}
-      <h2
+      <p
         css={css`
-          ${styles.TEXT_L};
-          color: ${styles.YELLOW_500};
+          ${styles.TEXT_R};
           word-break: keep-all;
-          
         `}
       >
-        재설정할 계정의 이메일 주소를 입력해 주세요
-      </h2>
+        재설정할 계정의 이메일 주소를 입력해주세요.
+      </p>
     </div>
   );
 };
 export const LostPassword: React.FC = () => {
   return (
-    <form
-      css={css`
-        width: calc(100% - 2 * ${styles.SPACE_XXL});
-        max-width: 272px;
-      `}
-    >
-      <PageHint />
-      <TextInput
-        id=""
-        type="email"
-        width="100%"
-        placeholder="email@example.com"
-        css={css`
-          margin-bottom: 16px;
-        `}
-      />
-      <Button hasBorder size="regular" width="100%">
-        이메일 주소 입력
-      </Button>
-    </form>
+    <React.Fragment>
+      <ButtonWrapper position="top-left" padding="x-small">
+        <Link to=".">
+          <Button size="large">
+            <icons.ArrowLeft />
+          </Button>
+        </Link>
+      </ButtonWrapper>
+      
+      <form css={css`width: calc(100% - 2 * ${styles.SPACE_L});`}>
+        <PageHint />
+        <div css={css`
+          width: 100%;
+          max-width: 272px;
+          margin: 0 auto;
+        `}>
+          <TextInput
+            id=""
+            type="email"
+            width="272px"
+            placeholder="email@example.com"
+            css={css`
+              margin-bottom: 16px;
+            `}
+          />
+          <Button hasBorder size="regular" width="100%">
+            이메일 주소 입력
+          </Button>
+        </div>
+      </form>
+    </React.Fragment>
   );
 };
 
