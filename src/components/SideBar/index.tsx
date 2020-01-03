f28a85149590486f1f16ef4bd0116957490845ce
Doro-nyong / 도로뇽 <doronyong94@gmail.com>
2020-01-03 19:25:28 +0900

    add: media query at BottomBar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -9,7 +9,7 @@ import HashTagList from './HashTagList';
 import PlaceNav from './PlaceNav';
 import BottomSetting from './BottomSetting';
 
-const SideBarStyle = css`
+const SideBarWrapperStyle = css`
   z-index: 20;
 
   ${styles.TRANSITION_SLOW}
@@ -32,13 +32,43 @@ const SideBarStyle = css`
   }
 `;
 
+const SideBarStyle = css`
+  position: relative;
+`;
+
+const SideBarMenuWrapperStyle = css`
+  ${styles.TRANSITION_SLOW}
+  transition-property: left;
+
+  position: fixed;
+  top: 250px;
+  left: 0;
+  bottom: 80px;
+
+  width: 256px;
+
+  overflow-x: auto;
+
+  ${styles.MEDIAQUERY_UNDER_R} {
+    left: -256px;
+
+    body.sidebar-opened & {
+      left: 0;
+    }
+  }
+`;
+
 export const SideBar: React.FC = () => {
   return (
-    <div css={SideBarStyle}>
-      <CurrentChar characterName="캐릭터의 이름" characterJob="캐릭터의 직업" />
-      <HashTagList />
-      <PlaceNav />
-      <BottomSetting />
+    <div css={SideBarWrapperStyle}>
+      <div css={SideBarStyle}>
+        <CurrentChar characterName="캐릭터의 이름" characterJob="캐릭터의 직업" />
+          <div css={SideBarMenuWrapperStyle}>
+            <HashTagList />
+            <PlaceNav />
+          </div>
+        <BottomSetting />
+      </div>
     </div>
   );
 };
