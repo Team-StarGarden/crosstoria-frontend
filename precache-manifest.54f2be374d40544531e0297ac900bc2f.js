5e992853d8ac6d5877d34f260ff0c2a9f8e43a0a
Alex4386 <Alex4386@users.noreply.github.com>
2020-03-02 20:40:06 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.54f2be374d40544531e0297ac900bc2f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d78bdc0417b6e0bcb49975cd9568c36",
+    "url": "https://team-stargarden.github.io/crosstoria-frontend/index.html"
+  },
+  {
+    "revision": "5aa0a58a8015d8695de8",
+    "url": "https://team-stargarden.github.io/crosstoria-frontend/static/css/main.842b75fd.chunk.css"
+  },
+  {
+    "revision": "5329fe7b478a193b1353",
+    "url": "https://team-stargarden.github.io/crosstoria-frontend/static/js/2.5b932a64.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "https://team-stargarden.github.io/crosstoria-frontend/static/js/2.5b932a64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5aa0a58a8015d8695de8",
+    "url": "https://team-stargarden.github.io/crosstoria-frontend/static/js/main.03ad0e07.chunk.js"
+  },
+  {
+    "revision": "4b1aca58e5593578c0fd",
+    "url": "https://team-stargarden.github.io/crosstoria-frontend/static/js/runtime-main.c927cf0a.js"
+  },
+  {
+    "revision": "0446389f529c381cc4c4ccfbafc91d37",
+    "url": "https://team-stargarden.github.io/crosstoria-frontend/static/media/bg_townmap.0446389f.jpg"
+  },
+  {
+    "revision": "ba516e6c15a872ea0097d87f16508f16",
+    "url": "https://team-stargarden.github.io/crosstoria-frontend/static/media/bg_worldmap.ba516e6c.jpg"
+  },
+  {
+    "revision": "21e6025929549b0c4652cb09fec60ae2",
+    "url": "https://team-stargarden.github.io/crosstoria-frontend/static/media/logo.21e60259.png"
+  },
+  {
+    "revision": "cfb0e13a70cbea29616f00daabf74475",
+    "url": "https://team-stargarden.github.io/crosstoria-frontend/static/media/logo_withtypo.cfb0e13a.png"
+  }
+]);
---
+++ storybook/precache-manifest.5ef16100764b11f426e4ef8fca990b1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f404858306b4e6e46a2d3aa394fc365",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.78f0092fd1ebaec5dec5.bundle.js"
+  },
+  {
+    "url": "runtime~main.78f0092fd1ebaec5dec5.bundle.js"
+  },
+  {
+    "revision": "ba516e6c15a872ea0097d87f16508f16",
+    "url": "static/media/bg_worldmap.ba516e6c.jpg"
+  },
+  {
+    "revision": "21e6025929549b0c4652cb09fec60ae2",
+    "url": "static/media/logo.21e60259.png"
+  },
+  {
+    "revision": "cfb0e13a70cbea29616f00daabf74475",
+    "url": "static/media/logo_withtypo.cfb0e13a.png"
+  },
+  {
+    "revision": "f1f5a07a1ce4e863480d6025b958e6f5",
+    "url": "static/media/reset.f1f5a07a.css"
+  },
+  {
+    "url": "vendors~main.78f0092fd1ebaec5dec5.bundle.js"
+  },
+  {
+    "url": "vendors~main.78f0092fd1ebaec5dec5.bundle.js.LICENSE"
+  }
+]);
